8b4e730af8a116d4e248664c7621938ecc4069e9
Mzeeha <mzeemzima099@gmail.com>
2025-01-27 16:13:29 +0300

    Initial commit

---
+++ CONFIG_DB.py
@@ -0,0 +1,24 @@
+import pymongo
+
+client = pymongo.MongoClient(
+        "set-your-mongo-url"
+)
+
+result = str(client)
+
+if "connect=True" in result:
+    try:
+        print("CONFIG DB CONNECTED SUCCESSFULLY ✅")
+    except:
+        pass
+else:
+    try:
+        print("CONFIG DB CONNECTION FAILED ❌")
+    except:
+        pass
+
+
+COLLECTIONS = client["CONFIG_DATABASE"]
+BLACKLISTED_SKS = COLLECTIONS.BLACKLISTED_SKS
+TOKEN_DB = COLLECTIONS.TOKEN_DB
+SKS_DB = COLLECTIONS.SKS_DB
---
+++ mongodb.py
@@ -0,0 +1,26 @@
+import traceback
+import pymongo
+
+client = pymongo.MongoClient(
+        "set-your-mongo-url"
+
+)
+result = str(client)
+
+if "connect=True" in result:
+    try:
+        print("MONGODB CONNECTED SUCCESSFULLY ✅")
+    except:
+        pass
+else:
+    try:
+        print("MONGODB CONNECTION FAILED ❌")
+    except:
+        pass
+
+folder = client["MASTER_DATABASE"]
+usersdb = folder.USERSDB
+chats_auth = folder.CHATS_AUTH
+gcdb = folder.GCDB
+sksdb = client["SKS_DATABASE"].SKS
+confdb = client["SKS_DATABASE"].CONF_DATABASE
