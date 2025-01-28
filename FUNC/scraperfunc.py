bc0e02cd81a33790afe7a357dceac06f2b9ba898
goonkenya <darkwebtechke7@gmail.com>
2025-01-28 23:29:49 +0300

    Update scraperfunc.py

--- FUNC/scraperfunc.py
+++ FUNC/scraperfunc.py
@@ -69,7 +69,7 @@ Message: We Didn't Find Any CC In @{channel_link}.
 </b>"""
             await message.reply_text(resp, message.id)
         else:
-            file_name = f"downloads/{amt}x_CC_Scraped_For_{message.from_user.id}_By_@MASTERChecker.txt"
+            file_name = f"downloads/{amt}x_CC_Scraped_For_{message.from_user.id}_By_@checksbotke.txt"
             with open(file_name, "a", encoding="utf-8") as f:
                 for x in ccs:
                     cc, mes, ano, cvv = x[0], x[1], x[2], x[3]
@@ -95,7 +95,7 @@ CC Scraped ✅
 ● Duplicate Removed: {duplicate}
 ● Scraped By: <a href="tg://user?id={message.from_user.id}"> {message.from_user.first_name}</a> ♻️ [ {role} ]
 ● Time Taken: {min} Minutes {sec} Seconds
-● Bot by - <a href="tg://user?id=6745804180">Toͥnmͣoͫy 〔 Ɠφ 〕</a>
+● Bot by - <a href="tg://user?id=6622603977">nairobiangoon〔 Ɠφ 〕</a>
 """
             await message.reply_document(document=file_name, caption=caption, reply_to_message_id=message.id)
             os.remove(file_name)
@@ -178,7 +178,7 @@ Message: We Didn't Find Any CC In {channel_title}.
             await message.reply_text(resp, message.id)
 
         else:
-            file_name = f"downloads/{amt}x_CC_Scraped_For_{message.from_user.id}_By_@MASTERϟ.txt"
+            file_name = f"downloads/{amt}x_CC_Scraped_For_{message.from_user.id}_By_@nairobiangoon.txt"
             with open(file_name, "a", encoding="UTF-8") as f:
                 for x in ccs:
                     cc, mes, ano, cvv = x[0], x[1], x[2], x[3]
@@ -198,7 +198,7 @@ CC Scraped ✅
 ● Duplicate Removed: {dublicate}
 ● Scraped By: <a href="tg://user?id={message.from_user.id}"> {message.from_user.first_name}</a> ♻️ [ {role} ]
 ● Time Taken: {min} Minutes {sec} Seconds
-● Bot by - <a href="tg://user?id=6745804180">Toͥnmͣoͫy 〔 Ɠφ 〕</a>
+● Bot by - <a href="tg://user?id=6622603977">Toͥnmͣoͫy 〔 Ɠφ 〕</a>
 </b>
 """
             await message.reply_document(document=file_name, caption=caption, reply_to_message_id=message.id)
@@ -250,7 +250,7 @@ Message: We Didnt Find Any CC In @{channel_link} .
             await message.reply_text(resp, message.id)
 
         else:
-            file_name = f"downloads/{amt}x_CC_Scraped_For_{message.from_user.id}_By_@MASTERϟ.txt"
+            file_name = f"downloads/{amt}x_CC_Scraped_For_{message.from_user.id}_By_@nairobiangoon.txt"
             with open(file_name, "a", encoding="UTF-8") as f:
                 for x in ccs:
                     cc, mes, ano, cvv = x[0], x[1], x[2], x[3]
@@ -275,7 +275,7 @@ CC Scraped ✅
 ● Duplicate Removed: {dublicate}
 ● Scraped By: <a href="tg://user?id={message.from_user.id}"> {message.from_user.first_name}</a> ♻️ [ {role} ]
 ● Time Taken: {min} Minutes {sec} Seconds
-● Bot by - <a href="tg://user?id=6745804180">Toͥnmͣoͫy 〔 Ɠφ 〕</a>
+● Bot by - <a href="tg://user?id=6622603977">Toͥnmͣoͫy 〔 Ɠφ 〕</a>
 """
             await message.reply_document(document=file_name, caption=caption, reply_to_message_id=message.id)
             os.remove(file_name)
@@ -334,7 +334,7 @@ Message: We Didnt Find Any CC In {channel_title} .
             await message.reply_text(resp, message.id)
 
         else:
-            file_name = f"downloads/{amt}x_CC_Scraped_For_{message.from_user.id}_By_@MASTERϟ.txt"
+            file_name = f"downloads/{amt}x_CC_Scraped_For_{message.from_user.id}_By_@nairobiangoon.txt"
             with open(file_name, "a", encoding="UTF-8") as f:
                 for x in ccs:
                     cc, mes, ano, cvv = x[0], x[1], x[2], x[3]
@@ -355,7 +355,7 @@ CC Scraped ✅
 ● Duplicate Removed: {dublicate}
 ● Scraped By: <a href="tg://user?id={message.from_user.id}"> {message.from_user.first_name}</a> ♻️ [ {role} ]
 ● Time Taken: {min} Minutes {sec} Seconds
-● Bot by - <a href="tg://user?id=6745804180">Toͥnmͣoͫy 〔 Ɠφ 〕</a>
+● Bot by - <a href="tg://user?id=6622603977">Toͥnmͣoͫy 〔 Ɠφ 〕</a>
 """
             await message.reply_document(document=file_name, caption=caption, reply_to_message_id=message.id)
             os.remove(file_name)
@@ -403,7 +403,7 @@ Message: We Didnt Find Any SK In @{channel_link} .
             await message.reply_text(resp, message.id)
 
         else:
-            file_name = f"downloads/{amt}x_SK_Scraped_For_{message.from_user.id}_By_@MASTERϟ.txt"
+            file_name = f"downloads/{amt}x_SK_Scraped_For_{message.from_user.id}_By_@nairobiangoon.txt"
             with open(file_name, "a", encoding="UTF-8") as f:
                 for x in ccs:
                     f.write(f"{x}\n")
@@ -424,7 +424,7 @@ SK Scraped ✅
 ● SK Found: {amt}
 ● Scraped By: <a href="tg://user?id={message.from_user.id}"> {message.from_user.first_name}</a> ♻️ [ {role} ]
 ● Time Taken: {min} Minutes {sec} Seconds
-● Bot by - <a href="tg://user?id=6745804180">Toͥnmͣoͫy 〔 Ɠφ 〕</a>
+● Bot by - <a href="tg://user?id=6622603977">Toͥnmͣoͫy 〔 Ɠφ 〕</a>
 """
             await message.reply_document(document=file_name, caption=caption, reply_to_message_id=message.id)
             os.remove(file_name)
@@ -480,7 +480,7 @@ Message: We Didnt Find Any SK In {channel_title} .
             await message.reply_text(resp, message.id)
 
         else:
-            file_name = f"downloads/{amt}x_SK_Scraped_For_{message.from_user.id}_By_@MASTERϟ.txt"
+            file_name = f"downloads/{amt}x_SK_Scraped_For_{message.from_user.id}_By_@Mnairobiangoon.txt"
             with open(file_name, "a", encoding="UTF-8") as f:
                 for x in ccs:
                     f.write(f"{x}\n")
@@ -498,7 +498,7 @@ SK Scraped ✅
 ● Duplicate Removed: {dublicate}
 ● Scraped By: <a href="tg://user?id={message.from_user.id}"> {message.from_user.first_name}</a> ♻️ [ {role} ]
 ● Time Taken: {min} Minutes {sec} Seconds
-● Bot by - <a href="tg://user?id=6745804180">Toͥnmͣoͫy 〔 Ɠφ 〕</a>
+● Bot by - <a href="tg://user?id=6622603977">Toͥnmͣoͫy 〔 Ɠφ 〕</a>
 """
             await message.reply_document(document=file_name, caption=caption, reply_to_message_id=message.id)
             os.remove(file_name)
