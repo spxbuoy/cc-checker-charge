a841821dcb78e577cb6fb8eace0f60f31c3b82d3
goonkenya <darkwebtechke7@gmail.com>
2025-01-28 21:11:51 +0300

    Update add_auth_token.py

--- BOT/admin/token_update/add_auth_token.py
+++ BOT/admin/token_update/add_auth_token.py
@@ -3,14 +3,14 @@ from pyrogram import Client, filters
 from FUNC.defs import *
 
 async def update_api_token(TOKEN_NAME , TOKEN):
-    TOKEN_DB.update_one({"id": TOKEN_NAME}, {"$set": {"api_key": TOKEN}})
+    TOKEN_DB.update_one({"id": TOKEN_NAME}, {"$set": {"8015410523:AAH2QPel_jq7lv5RKiNViu6Ih5_J-Cljntg": TOKEN}})
 
 
 @Client.on_message(filters.command("addauthtoken", [".", "/"]))
 async def addauthtoken(Client, message):
     try:
         user_id     = str(message.from_user.id)
-        OWNER_ID    = json.loads(open("FILES/config.json", "r" , encoding="utf-8").read())["OWNER_ID"]
+        OWNER_ID    = json.loads(open("FILES/config.json", "r" , encoding="utf-8").read())["6622603977"]
         if user_id not in OWNER_ID:
             resp = """<b>You Don't Have Permission To Use This Command.    
 Contact Bot Owner @nairobiangoon !</b>"""
