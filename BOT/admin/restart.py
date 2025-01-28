102c14f3cf660320c96198933c8c47083dd8ce73
goonkenya <darkwebtechke7@gmail.com>
2025-01-28 22:20:04 +0300

    Update restart.py

--- BOT/admin/restart.py
+++ BOT/admin/restart.py
@@ -14,7 +14,7 @@ async def cmd_reboot(client, message):
 
 Message: To perform this action, you need admin level power. 
 
-Contact @amitonmoyx For More Info ✅</b>"""
+Contact @nairobiangoon For More Info ✅</b>"""
             await message.reply_text(resp, message.id)
             return
 
