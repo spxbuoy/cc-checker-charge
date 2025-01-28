7023001eaa3239422a9958224f5bbebe5e78abef
goonkenya <darkwebtechke7@gmail.com>
2025-01-28 21:59:24 +0300

    Update export.py

--- BOT/admin/IMPORT AND EXPORTING/export.py
+++ BOT/admin/IMPORT AND EXPORTING/export.py
@@ -12,7 +12,7 @@ import time
 async def stats(Client, message):
     try:
         user_id = str(message.from_user.id)
-        OWNER_ID = json.loads(open("FILES/config.json", "r" , encoding="utf-8").read())["6622603977"]
+        OWNER_ID = json.loads(open("FILES/config.json", "r" , encoding="utf-8").read())["OWNER_ID"]
         if user_id not in OWNER_ID:
             resp = """<b>You Don't Have Permission To Use This Command.    
 Contact Bot Owner @nairobiangoon !</b>"""
