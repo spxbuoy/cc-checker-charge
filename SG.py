47ca84829d5d9f8afa401944e7882203ca05dc45
goonkenya <nairobiangoon982@gmail.com>
2025-02-02 05:53:09 +0300

    Update SG.py

--- SG.py
+++ SG.py
@@ -3,10 +3,10 @@ from pyrogram import Client
 
 with open("FILES/config.json", "r",encoding="utf-8") as f:
     DATA         = json.load(f)
-    API_ID       = DATA["24872155"]
-    API_HASH     = DATA["36560cde182f779d6fe129dcd1d8f527"]
-    BOT_TOKEN    = DATA["8015410523:AAH2QPel_jq7lv5RKiNViu6Ih5_J-Cljntg"]
-    PHONE_NUMBER = DATA["+254799505823"]
+    API_ID       = DATA["API_ID"]
+    API_HASH     = "API_HASH"]
+    BOT_TOKEN    = "BOT_TOKEN"]
+    PHONE_NUMBER = DATA["PHONE_NUMBER"]
 
 user = Client("Scrapper",
               api_id       = API_ID,
